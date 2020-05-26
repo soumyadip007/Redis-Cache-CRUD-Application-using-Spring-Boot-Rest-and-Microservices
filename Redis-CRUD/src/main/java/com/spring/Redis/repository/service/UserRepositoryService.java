f5bd4e5c727085683bdb03ca493619f4f4820623
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 21:12:20 +0530

    All Service method added

--- Redis-CRUD/src/main/java/com/spring/Redis/repository/service/UserRepositoryService.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/repository/service/UserRepositoryService.java
@@ -1,11 +1,14 @@
 package com.spring.Redis.repository.service;
 
+import java.util.Map;
+
 import org.springframework.data.redis.core.HashOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 
 import com.spring.Redis.entity.User;
+import com.spring.Redis.repository.UserRepository;
 
-public class UserRepositoryService {
+public class UserRepositoryService implements UserRepository {
 
 	
 	private RedisTemplate<String, User> redisTemplate;
@@ -15,5 +18,31 @@ public class UserRepositoryService {
     public UserRepositoryService(RedisTemplate<String, User> redisTemplate) {
         this.redisTemplate = redisTemplate;
         hashOperations = redisTemplate.opsForHash();
+    }
+
+    @Override
+    public void save(User user) {
+        hashOperations.put("USER", user.getId(), user);
+    }
+
+    @Override
+    public Map<String, User> findAll() {
+        return hashOperations.entries("USER");
+    }
+
+    @Override
+    public User findById(String id) {
+        return (User)hashOperations.get("USER", id);
+    }
+
+    @Override
+    public void update(User user) {
+        save(user);
+    }
+
+    @Override
+    public void delete(String id) {
+
+        hashOperations.delete("USER", id);
     }
 }
