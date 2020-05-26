0de8138b72397d753b32bf65769effb810ede727
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 21:14:50 +0530

    Redis controller added

--- Redis-CRUD/src/main/java/com/spring/Redis/controller/RedisController.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/controller/RedisController.java
@@ -1,30 +1,48 @@
 package com.spring.Redis.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.redis.core.StringRedisTemplate;
+import java.util.Map;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.spring.Redis.entity.User;
+import com.spring.Redis.repository.UserRepository;
+
 @RequestMapping("/rest/url")
 @RestController
 public class RedisController {
 
-	 @Autowired
-	 StringRedisTemplate redisTemplate;
-	 
+	   private UserRepository userRepository;
 
-	    @GetMapping("/{id}")
-	    public String getUrl(@PathVariable String id) {
+	    public RedisController(UserRepository userRepository) {
+	        this.userRepository = userRepository;
+	    }
 
-	        String url = redisTemplate.opsForValue().get(id);
-	        System.out.println("URL Retrieved: " + url);
+	    @GetMapping("/add/{id}/{name}")
+	    public User add(@PathVariable("id") final String id,
+	                    @PathVariable("name") final String name) {
+	        userRepository.save(new User(id, name, 20000L));
+	        return userRepository.findById(id);
+	    }
 
-	        if (url == null) {
-	            throw new RuntimeException("There is no shorter URL for : " + id);
-	        }
-	        return url;
+	    @GetMapping("/update/{id}/{name}")
+	    public User update(@PathVariable("id") final String id,
+	                       @PathVariable("name") final String name) {
+	        userRepository.update(new User(id, name, 1000L));
+	        return userRepository.findById(id);
+	    }
+
+	    @GetMapping("/delete/{id}")
+	    public Map<String, User> delete(@PathVariable("id") final String id) {
+	        userRepository.delete(id);
+	        return all();
+	    }
+
+	    @GetMapping("/all")
+	    public Map<String, User> all() {
+	        return userRepository.findAll();
 	    }
 	    
 }
