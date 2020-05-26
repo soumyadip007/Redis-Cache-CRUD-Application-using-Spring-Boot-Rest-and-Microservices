db7812e07b49db6679a02d4b666a2a1c958060af
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 21:16:48 +0530

    Component scan added

--- Redis-CRUD/src/main/java/com/spring/Redis/RedisApplication.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/RedisApplication.java
@@ -2,13 +2,19 @@ package com.spring.Redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 import com.spring.Redis.entity.User;
 
 @SpringBootApplication
+@EnableAsync
+@ComponentScan({"com.spring.Redis.*"})
+@EntityScan("com.spring.Redis.*")
 public class RedisApplication {
 
 	public static void main(String[] args) {
