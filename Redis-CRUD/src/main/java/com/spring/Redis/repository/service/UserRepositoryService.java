9fdc4737f8e57e0621c1a0565bcdb571fc4c2537
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 21:09:28 +0530

    redis added

--- Redis-CRUD/src/main/java/com/spring/Redis/repository/service/UserRepositoryService.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/repository/service/UserRepositoryService.java
@@ -1,5 +1,19 @@
 package com.spring.Redis.repository.service;
 
+import org.springframework.data.redis.core.HashOperations;
+import org.springframework.data.redis.core.RedisTemplate;
+
+import com.spring.Redis.entity.User;
+
 public class UserRepositoryService {
 
+	
+	private RedisTemplate<String, User> redisTemplate;
+
+    private HashOperations hashOperations;
+
+    public UserRepositoryService(RedisTemplate<String, User> redisTemplate) {
+        this.redisTemplate = redisTemplate;
+        hashOperations = redisTemplate.opsForHash();
+    }
 }
