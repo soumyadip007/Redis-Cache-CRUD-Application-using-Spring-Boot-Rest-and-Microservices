1b8ee7c62fb6610b3f615120c2c4c6f513aeda3b
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 20:57:24 +0530

    JedisConnectionFactory added

--- Redis-CRUD/src/main/java/com/spring/Redis/RedisApplication.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/RedisApplication.java
@@ -2,12 +2,19 @@ package com.spring.Redis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
 
 @SpringBootApplication
 public class RedisApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
+	}
+	
+	@Bean
+	JedisConnectionFactory jedisConnectionFactory(){
+		return new JedisConnectionFactory();
 	}
 
 }
