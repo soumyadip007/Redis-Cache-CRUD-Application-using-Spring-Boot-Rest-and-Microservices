be5ce0537a5c1cf8727f751132db4c60fc37b4e6
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 21:06:07 +0530

    Methods added

--- Redis-CRUD/src/main/java/com/spring/Redis/repository/UserRepository.java
+++ Redis-CRUD/src/main/java/com/spring/Redis/repository/UserRepository.java
@@ -1,5 +1,18 @@
 package com.spring.Redis.repository;
 
+import java.util.Map;
+
+import com.spring.Redis.entity.User;
+
 public interface UserRepository {
 
+    void save(User user);
+    
+    Map<String, User> findAll();
+    
+    User findById(String id);
+    
+    void update(User user);
+    
+    void delete(String id);
 }
